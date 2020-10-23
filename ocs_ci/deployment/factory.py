35529ec3f409de14aab35284b3f430dd67a39c12
Pratik Surve <prsurve@redhat.com>
2020-10-23 12:53:52 +0530

    vmware.py fix
    Signed-off-by: Pratik Surve <prsurve@redhat.com>

--- ocs_ci/deployment/factory.py
+++ ocs_ci/deployment/factory.py
@@ -6,7 +6,7 @@ from ocs_ci.deployment.ibm import IBMDeployment
 from .aws import AWSIPI, AWSUPI, AWSUPIFlexy
 from .azure import AZUREIPI
 from .gcp import GCPIPI
-from .vmware import VSPHEREUPI
+from .vmware import VSPHEREUPI, VSPHEREIPI
 from .baremetal import BAREMETALUPI, BaremetalPSIUPI
 
 logger = logging.getLogger(__name__)
@@ -25,6 +25,7 @@ class DeploymentFactory(object):
             'aws_upi_flexy': AWSUPIFlexy,
             'azure_ipi': AZUREIPI,
             'vsphere_upi': VSPHEREUPI,
+            'vsphere_ipi': VSPHEREIPI,
             'baremetalpsi_upi_flexy': BaremetalPSIUPI,
             'baremetal_upi': BAREMETALUPI,
             'gcp_ipi': GCPIPI,
